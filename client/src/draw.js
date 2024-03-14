98f7057ff6b24920694497838c7392091e63e2b5
Rayane Belguebli <rayane.belguebli.etu@univ-lille.fr>
2024-03-14 16:55:53 +0100

    reAdd draw

---
+++ client/src/draw.js
@@ -0,0 +1,10 @@
+export default function draw(canvas, context, image, x, y) {
+	if (
+		x <= canvas.width - image.width &&
+		y <= canvas.height &&
+		x >= 0 &&
+		y >= 0
+	) {
+		context.drawImage(image, x, y);
+	}
+}
